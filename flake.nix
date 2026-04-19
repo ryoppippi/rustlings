9bd91612256f6f07baa9b57596486b086f4cd97f
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2026-04-19 20:48:45 +0100

    chore: init env

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ inputs.rust-overlay.overlays.default ];
+          };
+          cargoToml = fromTOML (builtins.readFile ./Cargo.toml);
+          rust = pkgs.rust-bin.stable.${cargoToml.package.rust-version}.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+              "rustfmt"
+            ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = [
+              rust
+              pkgs.rustlings
+            ];
+          };
+        };
+    };
+}
